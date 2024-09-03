70d83afecc563364499ec5fb50fdb831b28b82ff
davidius <davidmichael4d@gmail.com>
2024-09-02 20:42:56 -0500

    release version 0.1.1

--- Formula/flashcards.rb
+++ Formula/flashcards.rb
@@ -4,9 +4,9 @@
 class Flashcards < Formula
   desc "A simple flashcards app for the terminal"
   homepage "https://github.com/davidius/flashcards"
-  url "https://github.com/davidius/flashcards/releases/download/v0.1.0/flashcards.tar.gz"
-  sha256 "1ca05910da853c0cf530218925c87f060db48261fd0489fdc6c8502bd0ea6fe9"
-  version "0.1.0"
+  url "https://github.com/davidius/flashcards/releases/download/v0.1.1/flashcards.tar.gz"
+  sha256 "724cdcaf8870206d005c3f8cc7ba58a5e48177faace84cf12b2dd737f8395a28"
+  version "0.1.1"
 
   def install
     bin.install "flashcards"
